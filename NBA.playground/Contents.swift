8e8cd4df79bf98f28ba4eb2e764b50ea7359feac
mohamed <msalad@uncc.edu>
2017-03-08 03:26:35 -0500

    Added Cavaliers team into list

--- NBA.playground/Contents.swift
+++ NBA.playground/Contents.swift
@@ -8,10 +8,26 @@ import Cocoa
 //Create an NBA team class
 
 class NBA {
-    //Create optional strings for all the players in the league
-    var playerOne :String?
-    var playerTwo :String?
-    var playerThree :String?
+    //Tuple for all the players in the league
+    var tuplePlayers = (playerOne :NbaPlayer(), playerTwo :NbaPlayer(), playerThree :NbaPlayer()  )
+    
+    var wins: Int {
+        get {
+            return self.wins
+        }
+        set {
+            self.wins = newValue
+        }
+    }
+    
+    var losses: Int {
+        get {
+            return self.losses
+        }
+        set {
+            self.losses = newValue
+        }
+    }
     
 }
 
@@ -43,4 +59,33 @@ class NbaPlayer{
             self.height = newValue
         }
     }
+    
+    var ppg: Double {
+        get {
+            return self.ppg
+        }
+        set {
+            self.ppg = newValue
+        }
+    }
 }
+
+
+var league = NBA()
+var curry  = NbaPlayer()
+curry.name = "Stephen Curry"
+curry.height = 76
+curry.ppg = 55.8
+var dirk = NbaPlayer()
+dirk.name = "Dirk Nowizki"
+dirk.height = 84
+dirk.ppg = 33.4
+var love = NbaPlayer()
+love.name = "Kevin Love"
+love.height = 82
+love.ppg = 20.0
+
+league.tuplePlayers = (curry, dirk, love)
+print("dasda \(league.tuplePlayers.playerOne.name)")
+
+
