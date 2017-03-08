a8442c42c370e603b46a62cea284b62b481b7300
mohamed <msalad@uncc.edu>
2017-03-08 01:48:58 -0500

    Added function to increment tries

--- TenTries.playground/Contents.swift
+++ TenTries.playground/Contents.swift
@@ -3,3 +3,21 @@
 import Cocoa
 
 var str = "Hello, playground"
+var isLockedOut = false;
+var tries = 0
+
+
+func incrementTries(varIn : inout Int) -> Bool{
+    varIn = varIn + 1
+    if varIn == 10{
+    return true
+    }
+    return false
+}
+while( tries <= 10){
+    print("Try again")
+    //tries = tries + 1
+    incrementTries(varIn: &tries)
+}
+
+
