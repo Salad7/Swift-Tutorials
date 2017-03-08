1c02a023d19896cc41aa972e376ae894ce7f1224
mohamed <msalad@uncc.edu>
2017-03-08 03:09:33 -0500

    Created NBA project for storing NBA players data, created NBA player class with computed properties

---
+++ NBA.playground/Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+//Lets create an NBA team using swift.
+
+
+//Create an NBA team class
+
+class NBA {
+    //Create optional strings for all the players in the league
+    var playerOne :String?
+    var playerTwo :String?
+    var playerThree :String?
+    
+}
+
+//Class used to describe both NBA players and Dleague players and Overseas players
+class Personnel{
+    
+    
+    
+}
+
+class NbaPlayer{
+    //Players have names, heights and points per game
+    
+    
+    var name: String {
+        get{
+            return self.name
+        }
+        set{
+            self.name = newValue
+        }
+    }
+    
+    var height: Int {
+        get {
+            return self.height
+        }
+        set {
+            self.height = newValue
+        }
+    }
+}
