6fdd77a003fc8dce3c4b01e2ad70bfe852b3e3d6
mohamed <msalad@uncc.edu>
2017-05-15 18:28:48 -0400

    Created player class and added Player array

--- NBA.playground/Contents.swift
+++ NBA.playground/Contents.swift
@@ -1,1 +1,26 @@
 import Cocoa
+
+//Create NBA teams
+
+//Create players and stats
+
+
+
+
+class Player {
+    var name:String?
+    var points:Float?
+    var assists:Float?
+    var rebounds:Float?
+    init(n:String, p:Float, a:Float, r:Float){
+        name = n;
+        points = p;
+        assists = a;
+        rebounds = r;
+    }
+    
+    
+    var players = [Player](count: 30)
+    
+    
+}
